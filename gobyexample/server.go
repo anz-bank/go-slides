e0b8614c4017a073266a7aebfb3985b59595420d
Julia Ogris <julia.ogris@gmail.com>
2019-02-06 08:29:54 +1100

    :mag: Fix ListenAndServe error handling pattern

--- gobyexample/server.go
+++ gobyexample/server.go
@@ -9,8 +9,5 @@ import (
 func main() {
 	addr := ":4242"
 	fmt.Printf("Open your web browser and visit http://127.0.0.1%s\n", addr)
-	err := http.ListenAndServe(addr, http.FileServer(http.Dir(".")))
-	if err != nil {
-		log.Printf("Error running web server for static assets: %v", err)
-	}
+	log.Fatal(http.ListenAndServe(addr, http.FileServer(http.Dir("."))))
 }
