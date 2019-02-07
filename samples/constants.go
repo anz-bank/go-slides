e10e87f7a6da9bfb583ef8865bdb6cf301639cc1
Julia Ogris <julia.ogris@gmail.com>
2019-02-08 10:24:37 +1100

    Add iota to Const sample

--- samples/constants.go
+++ samples/constants.go
@@ -5,8 +5,18 @@ import "fmt"
 
 const Phi = 2.61803398875
 
+// The `iota` identifier is used in const declarations to
+// simplify definitions of incrementing numbers.
+const (
+	c0 = iota // c0 == 0
+	c1        // c1 == 1
+	c2        // c2 == 2
+)
+
 func main() {
 	const World = "ವಿಶ್ವ"
 	fmt.Println("Hello", World)
 	fmt.Println("Beauty in number:", Phi)
+
+	fmt.Println(c0, c1, c2)
 }
