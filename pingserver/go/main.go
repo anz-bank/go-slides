26657c8781d34878198211527191d52a405ecc74
Julia Ogris <julia.ogris@gmail.com>
2019-02-10 11:29:10 +1100

    Update times to Seconds from Millis

--- pingserver/go/main.go
+++ pingserver/go/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(2000 * time.Millisecond)
+		time.Sleep(2 * time.Second)
 		fmt.Fprint(w, "pong")
 	})
 	addr := ":9090"
