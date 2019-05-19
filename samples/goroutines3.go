698d7a4dc6dd5fa4c78fd3f7da72bb015725dd1f
Sai Kiran Gummaraj <saikiran.gummaraj@anz.com>
2019-05-19 11:25:07 +1000

    Update slides and samples for concurrency basics (#84)

---
+++ samples/goroutines3.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+var nGoroutines int
+
+func memFunc() {
+	b := make([]int, 1024)
+	for i := 0; i < len(b); i++ {
+		b[0] = i
+		time.Sleep(1 * time.Microsecond)
+	}
+}
+
+func report() {
+	for {
+		n := runtime.NumGoroutine()
+		if n > 1 && nGoroutines != n {
+			nGoroutines = n
+			fmt.Println("report: Number of goroutines:", n)
+		} else if n == 1 {
+			fmt.Println("No more goroutines")
+			break
+		}
+	}
+}
+
+func main() {
+
+	for i := 0; i < 100000; i++ {
+		if i%10000 == 0 {
+			fmt.Printf("Started %d threads\n", i)
+		}
+		go memFunc()
+	}
+	report()
+}
