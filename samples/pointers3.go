a65f438e8e5177250342f7363f2cbe22451ed7f6
Daniel Cantos <daniel.cantos@anz.com>
2019-05-20 08:14:59 +1000

    Add a sample for pointers saving the day
    Sample demonstrates how pointers behave in loops, particularly how
    a range over an array copies values into the loop variable. This
    causes raw structs to be unmodifiable, unless you loop over an
    array of pointers rather than raw structs.
    
    Also Adds link to relevant slide.

---
+++ samples/pointers3.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type MyStruct struct {
+	n int
+}
+
+func RawLoop(arr []MyStruct) {
+	// A loop over structs COPIES each array element into item
+	for _, item := range arr {
+		item.n = 1
+	}
+}
+
+func PointerLoop(arr []*MyStruct) {
+	// Looping over pointers copies the POINTER, which still points to
+	// the original object
+	for _, item := range arr {
+		item.n = 1
+	}
+}
+
+func main() {
+	// Looping over raw struct will not allow you to modify them
+	arr := []MyStruct{{0}, {0}, {0}}
+	RawLoop(arr)
+	for _, item := range arr {
+		fmt.Println(item.n)
+	}
+
+	fmt.Println()
+
+	// Looping over pointers fixes this
+	pointerArr := []*MyStruct{{0}, {0}, {0}}
+	PointerLoop(pointerArr)
+	for _, item := range pointerArr {
+		fmt.Println(item.n)
+	}
+}
