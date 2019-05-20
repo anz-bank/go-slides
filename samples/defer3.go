56b60fb0b2d84f3b4a047c08fbc6376d36c3dcad
Sai Kiran Gummaraj <saikiran.gummaraj@anz.com>
2019-05-20 12:37:28 +1000

    Implement review comments from anzboi (#77)

--- samples/defer3.go
+++ samples/defer3.go
@@ -2,12 +2,15 @@ package main
 
 import "fmt"
 
+// Defer functions are executed in LIFO order
 func f1() {
 	for i := 0; i < 3; i++ {
 		defer fmt.Print(i, " ")
 	}
 }
 
+// The arguments to the deferred function are evaluated when defer executes
+// not when the call executes
 func f2() {
 	for i := 0; i < 3; i++ {
 		defer func() {
@@ -16,6 +19,7 @@ func f2() {
 	}
 }
 
+// defer call executes after evaluating the return but before actually returning
 func f3() (result int) {
 	defer func() {
 		result *= 7
