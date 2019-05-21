1832b8cd046c18e42d6cd323bba41e1411811218
joshua carpeggiani <joshuacarpeggiani@gmail.com>
2019-05-21 13:29:16 +1000

    Redo with upto 20000 requests

--- pingserver/compare.py
+++ pingserver/compare.py
@@ -7,14 +7,16 @@ import os
 import time
 filename = "responsetime.csv"
 address = "localhost"
-request_range = [1000, 21000, 1000]
+request_range = [1000, 20000, 1000]
 
-def testport(address, port, numRequests, filename_full):
+def testport(address, port, numRequests):
     timeout = 2000
-    command = f"ab -n {numRequests} -c {numRequests} -s {timeout} -q -r -e {filename_full} {address}:{port}/ping"
+    command = f"ab -n {numRequests} -c {numRequests} -s {timeout} -q -r -e temp.csv {address}:{port}/ping"
     print(command)
-    check_output(command, shell = True)
-    return open(filename_full, "r")
+    os.system(command)
+    file = open("temp.csv", "r")
+    os.system("rm temp.csv")
+    return file
 
 def read_file(file):
     reader = pandas.read_csv(file)
@@ -27,25 +29,22 @@ def compare(filename, address, request_range):
     header = ["numRequests"] + ["go_"+ str(i) for i in range(0, 101, 1)] + ["java_"+ str(i) for i in range(0, 101, 1)]
     c.writerow (header)
     for numRequests in range(request_range[0], request_range[1], request_range[2]):
-        filename_full = f"responses/{filename}_9090_{numRequests}.csv"
-        f = testport(address, 9090, numRequests, filename_full)
+        f = testport(address, 9090, numRequests)
         go_list = read_file(f)
-
-        filename_full = f"responses/{filename}_8080_{numRequests}.csv"
-        f = testport(address, 8080, numRequests, filename_full)
+        f = testport(address, 8080, numRequests)
         java_list = read_file(f)
         c.writerow([numRequests] + go_list + java_list)
     file.close()
 
 def generate_graph(filename, request_range):
     data = pandas.read_csv(filename)
-    plt.figure(figsize=(10,6))
-    plt.plot(data.numRequests.tolist(), data.java_50.tolist(), "o", label = "Java Median", color='#ff9721')
-    plt.plot(data.numRequests.tolist(), data.java_90.tolist(), "^", label = "Java 90th percentile", color='#ff9721')
-    plt.plot(data.numRequests.tolist(), data.go_50.tolist(), "o", label = "Go Median", color = '#4f92ff')
-    plt.plot(data.numRequests.tolist(), data.go_90.tolist(), "^", label = "Go 90th percentile", color = '#4f92ff')
+    # plt.figure(figsize=(10,6))
+    plt.plot(data.numRequests.tolist(), data.java_50.div(1000).tolist(), "o", label = "Java Median", color='#ff9721')
+    plt.plot(data.numRequests.tolist(), data.java_95.div(1000).tolist(), "^", label = "Java 95th percentile", color='#ff9721')
+    plt.plot(data.numRequests.tolist(), data.go_50.div(1000).tolist(), "o", label = "Go Median", color = '#4f92ff')
+    plt.plot(data.numRequests.tolist(), data.go_95.div(1000).tolist(), "^", label = "Go 95th percentile", color = '#4f92ff')
     bottom, top = plt.ylim()
-    plt.ylim(top = top + 20)
+    # plt.ylim(top = top + 20)
     plt.title('Java vs Go server response time')
     plt.xlabel('Number of concurrent requests')
     plt.ylabel('Time taken (s)')
@@ -55,14 +54,5 @@ def generate_graph(filename, request_range):
 
 
 if os.path.isfile(filename) is False:
-    try:
-        check_output("netstat -an | grep 9090 | grep -i listen", shell = True)
-    except:
-        os.system("go run go/main.go > /dev/null &")
-    try:
-        check_output("netstat -an | grep 8080 | grep -i listen", shell = True)
-    except:
-        os.system("cd java && ./gradlew clean bootRun > /dev/null &")
-        time.sleep(10) # wait for the server to start
     compare(filename, address, request_range)
 generate_graph(filename, request_range)
