93ca256325c69b1149b218143a7bad422ee5762d
Daniel Cantos <daniel.cantos@anz.com>
2019-05-22 09:39:51 +1000

    Update test sample slide and cases test
    Moves the test samples slide to before lab 1. Also addes a link to
    Dave Cheney's article on table driven tests for attribution and
    more reading.
    
    Cases test updated to be different from Dave Cheney's example.

--- samples/testing/basic/cases_test.go
+++ samples/testing/basic/cases_test.go
@@ -1,29 +1,30 @@
 package main
 
 import (
-	"reflect"
-	"strings"
 	"testing"
 )
 
-func TestSplit(t *testing.T) {
+func IsEven(n int) bool {
+	return n%2 == 0
+}
+
+func TestIsEven(t *testing.T) {
 	// You can create many test cases succinctly with an anonymous struct array
 	testCases := map[string]struct {
-		input string
-		sep   string
-		want  []string
+		input int
+		want  bool
 	}{
-		"simple":       {input: "a b c", sep: " ", want: []string{"a", "b", "c"}},
-		"Wrong sep":    {input: "a/b/c", sep: " ", want: []string{"a/b/c"}},
-		"no sep":       {input: "abc", sep: " ", want: []string{"abc"}},
-		"trailing sep": {input: "a/b/c/", sep: "/", want: []string{"a", "b", "c", ""}},
+		"even":     {input: 132, want: true},
+		"odd":      {input: 3, want: false},
+		"0":        {input: 0, want: true},
+		"negative": {input: -43, want: false},
 	}
 
 	for name, test := range testCases {
 		// t.Run creates a sub test and runs it like a normal test
 		t.Run(name, func(t *testing.T) {
-			result := strings.Split(test.input, test.sep)
-			if !reflect.DeepEqual(result, test.want) {
+			result := IsEven(test.input)
+			if result != test.want {
 				t.Errorf("expected %v, got %v", test.want, result)
 			}
 		})
