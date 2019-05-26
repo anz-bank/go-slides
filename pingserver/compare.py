870966369ed6d70c5d530374daacac8b507c0431
joshcarp <joshuacarpeggiani@gmail.com>
2019-05-26 19:01:05 +1000

    Edit compare.py

--- pingserver/compare.py
+++ pingserver/compare.py
@@ -1,21 +1,21 @@
-from subprocess import check_output
-import re
-import matplotlib.pyplot as plt
 import csv
-import pandas
 import os
-import time
-filename = "responsetime.csv"
+
+import pandas
+import matplotlib.pyplot as plt
+
+responsetime_filename = "responsetime.csv"
 address = "localhost"
 request_range = [1000, 20000, 1000]
 
 def testport(address, port, numRequests):
+    temp_filename = "temp.csv"
     timeout = 2000
-    command = f"ab -n {numRequests} -c {numRequests} -s {timeout} -q -r -e temp.csv {address}:{port}/ping"
+    command = f"ab -n {numRequests} -c {numRequests} -s {timeout} -q -r -e {temp_filename} {address}:{port}/ping"
     print(command)
     os.system(command)
-    file = open("temp.csv", "r")
-    os.system("rm temp.csv")
+    file = open(temp_filename)
+    os.remove(temp_filename)
     return file
 
 def read_file(file):
@@ -23,8 +23,8 @@ def read_file(file):
     time_list = [reader.loc[i]["Time in ms"] for i in range(0, 101, 1)]
     return time_list
 
-def compare(filename, address, request_range):
-    file = open(filename, "w+")
+def compare(responsetime_filename, address, request_range):
+    file = open(responsetime_filename, "w+")
     c = csv.writer(file)
     header = ["numRequests"] + ["go_"+ str(i) for i in range(0, 101, 1)] + ["java_"+ str(i) for i in range(0, 101, 1)]
     c.writerow (header)
@@ -36,15 +36,13 @@ def compare(filename, address, request_range):
         c.writerow([numRequests] + go_list + java_list)
     file.close()
 
-def generate_graph(filename, request_range):
-    data = pandas.read_csv(filename)
+def generate_graph(responsetime_filename, request_range):
+    data = pandas.read_csv(responsetime_filename)
     plt.figure(figsize=(10,6))
     plt.plot(data.numRequests.tolist(), data.java_50.div(1000).tolist(), "o", label = "Java Median", color='#ff9721')
     plt.plot(data.numRequests.tolist(), data.java_95.div(1000).tolist(), "^", label = "Java 95th percentile", color='#ff9721')
     plt.plot(data.numRequests.tolist(), data.go_50.div(1000).tolist(), "o", label = "Go Median", color = '#4f92ff')
     plt.plot(data.numRequests.tolist(), data.go_95.div(1000).tolist(), "^", label = "Go 95th percentile", color = '#4f92ff')
-    bottom, top = plt.ylim()
-    # plt.ylim(top = top + 20)
     plt.title('Java vs Go server response time')
     plt.xlabel('Number of concurrent requests')
     plt.ylabel('Time taken (s)')
@@ -53,6 +51,6 @@ def generate_graph(filename, request_range):
     plt.savefig(os.path.abspath('..')+"/content/_img/JavaVsGoLoadTest.png", dpi=300, bbox_inches='tight', figsize=(50,25))
 
 
-if os.path.isfile(filename) is False:
-    compare(filename, address, request_range)
-generate_graph(filename, request_range)
+if not os.path.isfile(responsetime_filename):
+    compare(responsetime_filename, address, request_range)
+generate_graph(responsetime_filename, request_range)
